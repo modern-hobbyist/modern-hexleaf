5f898c892bd6df89dd78c19395c745f2a453e1f4
Charlie Steenhagen <csteen1005@gmail.com>
2019-12-02 20:16:34 -0500

    Added example credentials file

---
+++ credentials_example.h
@@ -0,0 +1,9 @@
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+// This will be emailed to you upon project creation
+char auth[] = "BLYNK AUTH CODE";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "My WiFi Connection";
+char pass[] = "examplePassword";
